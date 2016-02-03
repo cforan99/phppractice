9ccab4a83153e44444c3dbd933249b6494d48294
Christina Foran <cforan99@gmail.com>
2016-02-03 15:12:02 -0800

    added more words and deployed to edudeveloper.net/teeny.php

--- teeny.php
+++ teeny.php
@@ -1,3 +1,6 @@
+<!-- Check out teeny text live! 
+     edudeveloper.net/teeny.php -->
+
 <div style="text-align: center; font-family: sans-serif;">
 <h1 style="color: limegreen;">teeny text</h1>
 <hr>
@@ -6,10 +9,10 @@
 
 	if (isset($_POST['user_input'])&&!empty($_POST['user_input'])) {
 		$user_input = ' '.strtolower($_POST['user_input']);
-		$find = array(' thanks ', ' thank you ', ' please ', ' you ', ' for ', 'to', ' because ', ' of course ', 'be', ' by the way ', ' cool ', '.', ',', ' so', ' with ', ' later ', ' text ');
-		$replace = array(' thnx ', ' thnx ', ' plz ', ' u ', ' 4 ', '2', ' bc ', ' ofc ', 'b', ' btw ', ' kewl ', '', '', ' sooooo ', ' w ', ' l8r ', ' txt ');
+		$find = array(' thanks ', ' thank you ', ' please ', ' you ', ' for ', 'to', ' because ', ' of course ', 'be', ' by the way ', ' cool ', '.', ',', ' so', ' with ', ' later ', ' text ', ' see ');
+		$replace = array(' thnx ', ' thnx ', ' plz ', ' u ', ' 4 ', '2', ' bc ', ' ofc ', 'b', ' btw ', ' kewl ', '', '', ' sooooo ', ' w ', ' l8r ', ' txt ', ' c ');
 		$teeny = str_replace($find, $replace, $user_input);
-		echo $teeny;
+		echo $teeny."!";
 	} else {
 		echo "Please submit some text.";
 	}
